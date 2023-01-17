31250086237518433b7ad7db298f38d56cfd4658
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2023-01-16 19:47:30 -0500

    calculator.js

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -3,29 +3,58 @@
 // Ask the user for an operation to perform.
 // Perform the operation on the two numbers.
 // Print the result to the terminal.
+
 const readline = require('readline-sync');
 
-console.log("Welcome to Dave's Calculator");
-
-console.log("What's your first number?");
-let number1 = readline.question();
-
-console.log("What's your second number?");
-let number2 = readline.question();
-
-console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = readline.question();
-
-let output;
-if (operation === '1') {
-  output = Number(number1) + Number(number2);
-} else if (operation === '2'){
-  output = Number(number1) - Number(number2);
-} else if (operation === '3'){
-  output = Number(number1) * Number(number2);
-} else if (operation === '4'){
-  output = Number(number1) / Number (number2);
+function prompt(msg) {
+  console.log(`=> ${msg}`);
 }
 
-console.log(`The result is: ${output}`);
+function invalidNumber(num) {
+  return num.trimStart() === '' || Number.isNaN(Number(num));
+}
 
+prompt("Welcome to Dave's Calculator");
+
+prompt("What's your first number?");
+let number1 = readline.question();
+
+while (invalidNumber(number1)) {
+  prompt("Hmmmm...that doesn't look like a valid number.")
+  number1 = readline.question();
+}
+
+prompt("What's your second number?");
+let number2 = readline.question();
+
+while (invalidNumber(number2)) {
+  prompt("Hmmmm...that doesn't look like a valid number.")
+  number2 = readline.question();
+}
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("You must choose 1, 2, 3, 4.")
+  operation = readline.question();
+}
+
+let output;
+
+switch (operation) {
+  case '1': 
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+}
+
+prompt(`The result is ${output}`);
