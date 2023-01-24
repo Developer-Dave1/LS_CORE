6e2daaf56143f22d20042f4f31a379807cb12313
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2023-01-24 17:33:16 -0500

    adding easy2 question 2

--- lesson_3/easy2_2.js
+++ lesson_3/easy2_2.js
@@ -31,3 +31,16 @@ let sortedArray = [...numbers].sort((num1, num2) => num2 - num1);
 console.log(sortedArray); // [5, 4, 3, 2, 1]
 console.log(numbers); // [1, 2, 3, 4, 5]
 
+//USING FOR EACH METHOD
+
+let numbers = [1, 2, 3, 4, 5];
+let reversedArray = [];
+
+numbers.forEach((number) => {
+  reversedArray.unshift(number);
+});
+
+console.log(reversedArray); // [5, 4, 3, 2, 1]
+console.log(numbers); // [1, 2, 3, 4, 5]
+
+
