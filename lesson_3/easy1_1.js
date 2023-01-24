2f16c977fe6cec2f0b1c04f65f4e5f96a6093c95
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2023-01-24 13:53:00 -0500

    Adding first two files in lesson_3 easy 1

---
+++ lesson_3/easy1_1.js
@@ -0,0 +1,20 @@
+/* 
+Will this code raise an error?
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+
+This code will NOT return an error. JavaScript will leave empty slots for missing arugments in the index
+3-5, and then had the value "5" to index number [6]
+
+
+BONUS 
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+numbers[4];  // what will this line return?
+
+This line will not return anything, as that index referenced, [4], does not have a value.
+
+*/
+
