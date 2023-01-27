8fe6d75435ee84ea7e6e1905b68e6160bc4f22aa
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2023-01-26 19:05:44 -0500

    adding medium2

---
+++ lesson_3/medium2.js
@@ -0,0 +1,9 @@
+// return a new string that swaps the case of the letters
+
+let munstersDescription = "The Munsters are creepy and spooky.";
+
+let munstersT = munstersDescription.charAt(0).toLowerCase();
+let munstersCaps = munstersDescription.toUpperCase().slice(1);
+
+
+console.log(munstersT + munstersCaps);
