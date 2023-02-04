d2572b3df6e3b2c2fb3f09f362eda5be99265fe2
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2023-02-04 10:11:31 -0500

    adding easy1_7.js

---
+++ jssmallproblems/easy1_7.js
@@ -0,0 +1,50 @@
+/*
+
+write a function
+takes two string as arguments
+determines length of the two strings
+returns the result of concatenating the shorter string
+returns the result of the longer string 
+returns the result of the shorter string again
+
+*/
+
+function computeSum(targetNum) {
+  let total = 0;
+  for (let num = 1; num <= targetNum; num ++) {
+    total += num;
+  }
+
+  return total;
+}
+
+function computeProduct(targetNum) {
+  let total = 1;
+
+  for(let num = 1; num <= targetNum; num += 1) {
+    total *= num;
+  }
+
+  return total;
+
+}
+
+let readlineSync = require("readline-sync");
+
+console.log("Please enter an integer greater than 0");
+let number = parseInt(readlineSync.prompt(), 10);
+
+console.log("Enter 's' to compute the sum, 'p' to compute the product.");
+let operation = readlineSync.prompt();
+
+if (operation === "s") {
+  let sum = computeSum(number);
+  console.log(`The sum of the integers between 1 and ${number} is ${sum}.`);
+} else if (operation === "p") {
+  let product = computeProduct(number);
+  console.log(
+    `The product of the integers between 1 and ${number} is ${product}.`
+  );
+} else {
+  console.log("Oops. Unknown operation.");
+}
