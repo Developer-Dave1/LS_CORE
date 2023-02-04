204c4b410ee0114c52091a05c77916a41ad1eab0
Developer Dave <116127714+Developer-Dave1@users.noreply.github.com>
2023-02-04 18:40:04 -0500

    adding easy2_5.js

---
+++ jssmallproblems/easy2_5.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-sync");
+
+console.log("Enter the first number:");
+let num1 = Number(readlineSync.prompt());
+console.log("Enter the second number:");
+let num2 = Number(readlineSync.prompt());
+
+
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} * ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
+console.log(`${num1} % ${num2} = ${num1 % num2}`);
+console.log(`${num1} ** ${num2} = ${num1 ** num2}`);
+
